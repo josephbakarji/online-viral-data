2a9780c7490e2941826bbe0ca61781f9fd18335c
joseph bakarji <josephbakarji@gmail.com>
2020-03-24 10:35:04 -0700

    worldometers added a column for total deaths/1M population

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 MAINDIR = '/Users/josephbakarji/Documents/projects/coronadata/online-viral-data/'
-MAINTABLE_HEAD = ['Country', 'Total Cases', 'New Cases', 'Total Deaths', 'New Deaths', 'Total Recovered', 'Active Cases','Serious/Critical', 'Tot Cases/1M']
+MAINTABLE_HEAD = ['Country', 'Total Cases', 'New Cases', 'Total Deaths', 'New Deaths', 'Total Recovered', 'Active Cases','Serious/Critical', 'Tot Cases/1M', 'Tot Deaths/1M']
+
 SAVEFILE = MAINDIR + 'savedata.txt'
